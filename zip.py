e6283268eee886f4e769fc695b01aaa3c515987f
Andrew5205 <andrewchung11@gmail.com>
2020-09-23 20:45:19 -0700

    add zip.py

---
+++ zip.py
@@ -0,0 +1,25 @@
+# The zip() function returns a zip object, 
+# which is an iterator of tuples where the first item in each passed iterator is paired together, 
+# and then the second item in each passed iterator are paired together etc.
+
+numbers = [1, 2, 3]
+letters = ['a', 'b', 'c']
+
+zipped = zip(numbers, letters)
+print(zipped)                   # [(1, 'a'), (2, 'b'), (3, 'c')]
+print(type(zipped))             # <type 'list'>
+
+print(list(zipped))             # [(1, 'a'), (2, 'b'), (3, 'c')]
+
+
+
+# return type is list, NOT dictionary
+s1 = [2, 3, 1]
+s2 = ['b', 'a', 'c']
+
+unorderZipped = zip(s1, s2)
+print(unorderZipped)            # [(2, 'b'), (3, 'a'), (1, 'c')]
+
+# for k, v in unorderZipped.items():      # AttributeError: 'list' object has no attribute 'items'
+#     print(k)
+#     print(v)
